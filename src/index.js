dabbacd34fd4e5f6773cc9916a73bbc2d39d9b74
Tchilo <tchiloross@gmail.com>
2021-11-02 11:09:18 +0200

    Add function components

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import TodoContainer from './Components/TodoContainer';
-import './App.css'
+import TodoContainer from "./functionBased/Components/TodoContainer"
+import "./functionBased/App.css"
 
 ReactDOM.render(
   <React.StrictMode>
     <TodoContainer />
   </React.StrictMode>,
-  document.getElementById('root'),
+  document.getElementById('root')
 );
